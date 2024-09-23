3b6268116a72e959a60291ec65ca1d9471d0505f
TDMinhNhat <tdminhnhat13092003@gmail.com>
2024-09-23 08:12:01 +0700

    init eureka project

---
+++ travelling-app-be/src/main/java/dev/skyherobrine/app/TravellingAppBeApplication.java
@@ -0,0 +1,13 @@
+package dev.skyherobrine.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravellingAppBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravellingAppBeApplication.class, args);
+    }
+
+}
