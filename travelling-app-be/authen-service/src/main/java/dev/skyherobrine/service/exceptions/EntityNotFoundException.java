d7e82fb72f232c7c88769501257e29e474a942d0
TDMinhNhat <tdminhnhat13092003@gmail.com>
2024-09-23 09:20:53 +0700

    Authenticate: done API login

---
+++ travelling-app-be/authen-service/src/main/java/dev/skyherobrine/service/exceptions/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package dev.skyherobrine.service.exceptions;
+
+public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
