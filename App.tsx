b823e916d4d7c8c779c5fb2d8f15c36bac76f147
LMThu28 <minhthu281103@gmail.com>
2024-09-23 20:05:03 +0700

    Upload the Splash Screen interface

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar, View, Platform } from 'react-native'; 
-import Screen1 from './src/screens/screen1';  
+import Screen1 from './src/screens/Splash Screen';  
 
 export default function App() {
   return (
--- src/screens/Splash Screen.tsx
+++ src/screens/Splash Screen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Image, StatusBar } from 'react-native';
-import { styles } from '../styles/styleScreen1';  // Import styles từ file styles.js
+import { styles } from '../styles/style SplashScreen';  // Import styles từ file styles.js
 
 export default function App() {
   return (
@@ -8,7 +8,7 @@ export default function App() {
       <StatusBar barStyle="dark-content" />
       {/* Thêm logo */}
       <Image
-  source={require('../../assets/logo.png')}  // Sửa lại đường dẫn
+  source={require('../../assets/logo.png')}  
   style={styles.logo}
 />
 
