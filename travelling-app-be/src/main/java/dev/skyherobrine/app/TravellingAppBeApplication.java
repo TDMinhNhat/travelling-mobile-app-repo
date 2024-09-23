45060f6f7c4ec1e64263eb56cc416eebaa337117
TDMinhNhat <tdminhnhat13092003@gmail.com>
2024-09-23 08:13:32 +0700

    configure eureka

--- travelling-app-be/src/main/java/dev/skyherobrine/app/TravellingAppBeApplication.java
+++ travelling-app-be/src/main/java/dev/skyherobrine/app/TravellingAppBeApplication.java
@@ -2,8 +2,10 @@ package dev.skyherobrine.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class TravellingAppBeApplication {
 
     public static void main(String[] args) {
