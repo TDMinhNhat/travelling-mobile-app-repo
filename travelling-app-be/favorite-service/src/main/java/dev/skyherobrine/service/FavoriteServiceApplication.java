c9d19d94c586695932e948de59c8fe77d987ef13
TDMinhNhat <tdminhnhat13092003@gmail.com>
2024-09-23 09:04:17 +0700

    Favorite: init service

---
+++ travelling-app-be/favorite-service/src/main/java/dev/skyherobrine/service/FavoriteServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.skyherobrine.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FavoriteServiceApplication.class, args);
+    }
+
+}
