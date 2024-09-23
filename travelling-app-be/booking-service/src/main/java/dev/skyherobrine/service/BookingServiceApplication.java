4a16d50c3be86fd60a710274b0774784d5d7c421
TDMinhNhat <tdminhnhat13092003@gmail.com>
2024-09-23 09:14:35 +0700

    Booking: configure service

--- travelling-app-be/booking-service/src/main/java/dev/skyherobrine/service/BookingServiceApplication.java
+++ travelling-app-be/booking-service/src/main/java/dev/skyherobrine/service/BookingServiceApplication.java
@@ -2,12 +2,14 @@ package dev.skyherobrine.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BookingServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BookingServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BookingServiceApplication.class, args);
+	}
 
 }
