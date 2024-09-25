593cce111dcc63afc19d51e8d7832ac5c6730990
TDMinhNhat <tdminhnhat13092003@gmail.com>
2024-09-25 07:09:36 +0700

    display header in Login screen

--- travelling-app-fe/src/screens/LoginScreen.tsx
+++ travelling-app-fe/src/screens/LoginScreen.tsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, Text, View } from 'react-native';
+import LoginStyle from "../style/LoginStyle";
 
 export default function () {
 
     return (
-        <SafeAreaView>
-
+        <SafeAreaView style={LoginStyle.container}>
+            <View style={{width: "90%"}}>
+                <View style={{marginTop: 40}}>
+                    <Text style={{fontWeight: 800, fontSize: 20}}>Login Account</Text>
+                </View>
+                <View></View>
+                <View></View>
+            </View>
         </SafeAreaView>
     )
 }
--- travelling-app-fe/src/style/LoginStyle.tsx
+++ travelling-app-fe/src/style/LoginStyle.tsx
@@ -1,8 +1,11 @@
-import { StyleSheet } from "react-native";
+import { StatusBar, StyleSheet } from "react-native";
 
 export default StyleSheet.create({
     container: {
         width: "100%",
-        height: "100%"
+        height: "100%",
+        marginTop: StatusBar.currentHeight,
+        flex: 1,
+        alignItems: "center"
     }
 })
