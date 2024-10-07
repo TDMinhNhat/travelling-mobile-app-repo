344520b9aaef3ac6c733247396e722a4afc51429
Minh Thu <113564040+LMThu28@users.noreply.github.com>
2024-10-07 13:33:11 +0700

    fix library errors

--- travelling-app-fe/src/screens/CreateAccountScreen.tsx
+++ travelling-app-fe/src/screens/CreateAccountScreen.tsx
@@ -1,21 +1,24 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
-import CountryPicker, { Country, CountryCode } from 'react-native-country-picker-modal';  // Import CountryPicker
-import { Entypo } from '@expo/vector-icons'; // Import icon mũi tên chỉ xuống
+import CountryPicker, { Country, CountryCode } from 'react-native-country-picker-modal';
+import { Entypo } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons';
+import { LogBox } from 'react-native';
 import styles from '../style/styleCreateAccountScreen';
 
+// Tắt cảnh báo defaultProps
+LogBox.ignoreLogs(['Support for defaultProps']);
+
 const CreateAccountScreen = () => {
-  const [countryCode, setCountryCode] = useState<CountryCode>('VN');  // Mã quốc gia mặc định là Mỹ
-  const [callingCode, setCallingCode] = useState('84');   // Mã gọi điện mặc định là +1
+  const [countryCode, setCountryCode] = useState<CountryCode>('VN');
+  const [callingCode, setCallingCode] = useState('84');
 
   const onSelect = (country: Country) => {
-    // Kiểm tra country có hợp lệ trước khi cập nhật
     if (country && country.cca2 && country.callingCode) {
-      setCountryCode(country.cca2);  // Cập nhật mã quốc gia
-      setCallingCode(country.callingCode.length > 0 ? country.callingCode[0] : '');  // Kiểm tra và cập nhật mã gọi điện
+      setCountryCode(country.cca2);
+      setCallingCode(country.callingCode.length > 0 ? country.callingCode[0] : '');
     } else {
-      console.error('Invalid country data:', country);  // In ra lỗi nếu country không hợp lệ
+      console.error('Invalid country data:', country);
     }
   };
 
@@ -26,21 +29,19 @@ const CreateAccountScreen = () => {
       <Text style={styles.label}>Enter your mobile number:</Text>
 
       <View style={styles.inputRow}>
-        {/* Picker chọn quốc gia */}
         <View style={styles.countryPickerContainer}>
           <CountryPicker
             countryCode={countryCode}
             withFilter
             withFlag
             withCallingCode
-            withEmoji={false} // Không sử dụng biểu tượng emoji cho quốc gia
+            withEmoji={false}
             onSelect={onSelect}
-            withModal={true}  // Hiển thị modal quốc gia khi người dùng nhấn
+            withModal={true}
           />
           <Entypo name="chevron-down" size={20} color="black" style={styles.chevronIcon} />
         </View>
 
-        {/* Mã quốc gia + số điện thoại */}
         <TextInput
           style={styles.phoneInput}
           placeholder={`+${callingCode} Mobile number`}
